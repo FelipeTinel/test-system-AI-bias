62ed571105613b955527e9ba351aa8ee4f8a235b
FelipeTinel <felipemacedotinel@gmail.com>
2026-06-28 20:03:54 -0300

    feat: factory logic

--- api/app/services/factory.py
+++ api/app/services/factory.py
@@ -1,2 +1,21 @@
 from .ai_service import AIService
+from .openai_service import OpenAIService
 from .gemini_service import GeminiService
+from .groq_service import GroqService
+
+def get_ai_service (provider: str) -> str:
+
+    providers = {
+
+        "openai" : OpenAIService,
+        "gemini" : GeminiService,
+        "groq" : GroqService
+
+    }
+
+    provider_lower = provider.lower()
+
+    if provider_lower not in providers:
+        raise ValueError(f'Provedor {provider} não listado no sistema.')
+    
+    return providers[provider_lower]()
