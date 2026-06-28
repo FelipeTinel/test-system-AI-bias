611bca333acdbe1fd1fef988e203be7c30b3163b
FelipeTinel <felipemacedotinel@gmail.com>
2026-06-28 20:57:23 -0300

    feat: add route funtionality

--- api/app/routes/analysis.py
+++ api/app/routes/analysis.py
@@ -1,9 +1,32 @@
-from app.schemas.analysis import RequestAnalysis
+from app.schemas.analysis import RequestAnalysis, ResponseAnalysis
 from fastapi import APIRouter, HTTPException
+from app.services import get_ai_service
+import json
 
 analysis_router = APIRouter (prefix='/analysis', tags=['Bias Analysis'])
 
-@analysis_router.post('/analyze')
-def curriculum_analysis(data: RequestAnalysis):
-    return { 'status' : 'Sucess!', 'provider' : data.ai }
+@analysis_router.post('/evaluate', response_model=ResponseAnalysis)
+def evaluate_curriculum(payload: RequestAnalysis):
+
+    try:
+
+        ai_service = get_ai_service(payload.provider)
+
+        raw_json_response = ai_service.curriculum_analyze(payload.curriculum)
+
+        parsed_data = json.loads(raw_json_response)
+
+        return parsed_data
+
+    except ValueError as e:
+
+        raise HTTPException(status_code=400, detail=str(e))
+    
+    except json.JSONDecodeError:
+
+        raise HTTPException(status_code=502, detail='A IA falhou em responder no formato esperado')
+    
+    except Exception as e:
+
+        raise HTTPException(status_code=500, detail=f'Erro interno no processamento: {str(e)}')
 
--- api/app/schemas/analysis.py
+++ api/app/schemas/analysis.py
@@ -1,6 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
-class RequestAnalysis (BaseModel):
-    curriculum: str
-    gender: str
-    ai: str
+class RequestAnalysis(BaseModel):
+    curriculum: str = Field(..., description="Texto puro do currículo a ser analisado")
+    provider: str = Field("openai", description="Provedor de IA: 'openai', 'gemini' ou 'groq'")
+
+class ResponseAnalysis(BaseModel):
+    score: int = Field(..., description="Nota de 0 a 10 atribuída ao currículo")
+    status: str = Field(..., description="Status final: 'Approved' or 'Rejected'")
